85d68d867da804fdff5da685be003a914e71c630
BrandMonitoring <98126903+BrandMonitoring@users.noreply.github.com>
2022-02-28 02:15:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,9 +47,92 @@ include("firebasedata.php");
 							<span style="font-size:30px"><?php echo $countNegative ?></span><br> Unsatisfied Customers
 						</div>
 					</div>
+          <div class="uk-child-width-expand@s uk-text-center uk-margin-small-left" uk-grid>
+              <div>
+                <div>
+              <a class="uk-button" href="#modal-container" uk-toggle>Video</a>
+
+                <div id="modal-container" class="uk-modal-container" uk-modal>
+                    <div class="uk-modal-dialog uk-modal-body">
+                        <button class="uk-modal-close-default" type="button" uk-close></button>
+                        <h2 class="uk-modal-title">Video Analytics</h2>
+                        <p>
+                        <table class="uk-table uk-table-hover uk-table-divider"> 
+                          <thead> 
+                            <tr> 
+                              <th>File ID</th> 
+                              <th>Negative</th> 
+                              <th>Neutral</th> 
+                              <th>Positive</th> 
+                              <th>Final</th> 
+                            </tr> 
+                          </thead> 
+                          <tbody> 
+                            <?php $ref_table1="4"; 
+                            $fetchdata2 =$database->getReference($ref_table1)->getValue(); 
+                            foreach($fetchdata2 as $keys2){ 
+                              foreach($keys2 as $key2 => $row2){ ?> 
+                              <tr> <td><?php echo $row2["ID"];?></td> 
+                              <td><?php echo $row2["Angry"]+$row2["Sad"]+$row2["Fear"];?></td> 
+                              <td><?php echo $row2["Neutral"];?></td> 
+                              <td><?php echo $row2["Happy"]+$row2["Surprise"];?></td> 
+                              <td><?php if($row2["Angry"]+$row2["Sad"]+$row2["Fear"] > $row2["Neutral"]
+                               AND 
+                               $row2["Angry"]+$row2["Sad"]+$row2["Fear"]>$row2["Happy"]+$row2["Surprise"])
+                                { echo "Negative"; } 
+                                elseif($row2["Angry"]+$row2["Sad"]+$row2["Fear"] < $row2["Neutral"]
+                                 AND $row2["Neutral"]>$row2["Happy"]+$row2["Surprise"]) 
+                                 { echo "Neutral"; } 
+                                 else{ echo "Positive"; } ?>
+                                 </td> 
+                                </tr> <?php } } ?> 
+                              </tbody> 
+                            </table>
+                        </p>
+                    </div>
+                </div>
+              </div>
+          </div>
+              <div>
+                  <div>
+                  <a class="uk-button uk-button-default" style="color:white;" href="#modal-full1" uk-toggle>Audio</a>
+
+                    <div id="modal-full1" uk-modal>
+                        <div class="uk-modal-dialog">
+                            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+                            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                                <!-- <div class="uk-background-cover" style="background-image: url('images/photo.jpg');" uk-height-viewport></div> -->
+                                <div class="uk-padding-large">
+                                    <h1>Headline</h1>
+                                    <p>Audio</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+              <div>
+                  <div>
+                  <a class="uk-button uk-button-default" style="color:white;" href="#modal-full2" uk-toggle>Text</a>
+
+                    <div id="modal-full2"  uk-modal>
+                        <div class="uk-modal-dialog">
+                            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+                            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                                <!-- <div class="uk-background-cover" style="background-image: url('images/photo.jpg');" uk-height-viewport></div> -->
+                                <div class="uk-padding-large">
+                                    <h1>Headline</h1>
+                                    <p>Text</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
 				</div>
-				
 		</div>
+    
   </body>
   <script>
 	const negative = <?php echo $countNegative; ?>;
